71481ad36a67fd206dd97f3fc36a5b271b1515e7
nolan-sherman-susco <nolan.sherman@susco.net>
2020-04-07 16:14:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-xisisz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-xisisz)
---
+++ index.ts
@@ -0,0 +1,50 @@
+// Import stylesheets
+import './style.css';
+import { of, Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+//Setup a log function for logging.
+const appDiv: HTMLElement = document.getElementById('app');
+let log = (val:any) => {
+  let ele = document.createElement('p');
+  ele.append( JSON.stringify(val))
+  appDiv.appendChild(ele);
+}
+
+//Describe some basic types
+interface Login {
+  Username: string;
+  Password: string;
+}
+
+interface AuthToken {
+  Data:string;
+}
+
+interface Profile {
+  Name: string;
+  Birthday:string;
+}
+
+// The Following functions will simulate web request Observables.
+//Usually you will use some http client library to make a call to
+//the server. But that wont change how we interact with the observables.
+
+//Takes a login and gets an auth token from the server
+function authorize(login:Login): Observable<AuthToken>{
+  return of({Data: "aasiosof+&solfadshl93hksf"} as AuthToken).pipe(delay(500));
+}  
+
+//Gets profile data, requires a auth token.
+function getProfile(): Observable<Profile> {
+  return of({Name: "John Smith", Birthday:"04/01/1980"} as Profile).pipe(delay(500))
+}
+
+
+/** BEGIN Rxjs Tutorial */
+
+/**
+ * CHAPTER 1. 
+ * Using a single Observable
+ */
+
